368204fcc9ff80d862aab178959e39b7bcfeb483
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-07-10 00:18:56 +0600

    content settings

--- src/Pages/admin-pages/content/ContentManagement.jsx
+++ src/Pages/admin-pages/content/ContentManagement.jsx
@@ -15,8 +15,8 @@ import {
   AlertCircle,
   UploadCloud
 } from 'lucide-react';
-import StatCard from '../../../Components/admin-components/StatCard';
-import SectionCard from '../../../Components/admin-components/SectionCard';
+import StatCard from '../../../Components/admin-components/content/StatCard';
+import SectionCard from '../../../Components/admin-components/content/SectionCard';
 import { Link } from 'react-router-dom';
 import { Modal, Form, Input, Upload, message } from 'antd';
 
